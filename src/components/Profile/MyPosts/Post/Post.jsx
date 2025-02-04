b135265fcde59e3a058527b000b997635538a2e1
elizaweta128 <elizawetakompanienko@ukr.net>
2025-02-04 15:56:09 +0200

    deleted text

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -9,7 +9,6 @@ const Post = (props) => {
             <img src={avatar} alt=""/>
             {props.message}
             <div>
-                Hello
                 <span>like</span> {props.likesCount}
             </div>
         </div>
