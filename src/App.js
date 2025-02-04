a4e2edd9b47529a0050818b6145daf284ba6b29d
elizaweta128 <elizawetakompanienko@ukr.net>
2025-02-04 20:30:54 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ const Login = lazy(async () => {
     return {default: module.default};
 })
 //task_55
+//task_57
 class App extends React.Component {
     componentDidMount() {
         this.props.initializeApp();
